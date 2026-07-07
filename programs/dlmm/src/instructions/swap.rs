dea75757b4f19ded2568bf26b0ec761520bf3310
BibekPathak <bibekpathak101@gmail.com>
2026-07-07 18:29:51 +0530

    implement swap engine with bin traversal, ExactIn/ExactOut, and fees

--- programs/dlmm/src/instructions/swap.rs
+++ programs/dlmm/src/instructions/swap.rs
@@ -1,6 +1,14 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Token, TokenAccount, Transfer};
+use bytemuck::from_bytes_mut;
 use crate::state::*;
 use crate::errors::DlmmError;
+use crate::events::SwapEvent;
+use crate::state::bin_array::{bin_id_to_array_start, BINS_PER_ARRAY};
+use crate::math::price_math::bin_to_price;
+use crate::math::swap_math::compute_swap_step;
+use crate::math::fee_math::apply_bps;
+use crate::math::fixed_point::Q64;
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
 pub struct SwapParams {
@@ -19,16 +27,283 @@ pub struct Swap<'info> {
     #[account(mut)]
     pub pool: Account<'info, Pool>,
     #[account(mut)]
-    pub token_vault_a: UncheckedAccount<'info>,
+    pub token_vault_a: Account<'info, TokenAccount>,
     #[account(mut)]
-    pub token_vault_b: UncheckedAccount<'info>,
+    pub token_vault_b: Account<'info, TokenAccount>,
     #[account(mut)]
-    pub user_token_a: UncheckedAccount<'info>,
+    pub user_token_a: Account<'info, TokenAccount>,
     #[account(mut)]
-    pub user_token_b: UncheckedAccount<'info>,
-    pub token_program: Program<'info, anchor_spl::token::Token>,
+    pub user_token_b: Account<'info, TokenAccount>,
+    pub token_program: Program<'info, Token>,
 }
 
-pub fn handler(_ctx: Context<Swap>, _params: SwapParams) -> Result<()> {
-    Err(error!(DlmmError::NotImplemented))
+pub fn handler(ctx: Context<Swap>, params: SwapParams) -> Result<()> {
+    require!(params.amount > 0, DlmmError::ZeroDeposit);
+    if params.exact_in {
+        require!(params.min_amount_out > 0, DlmmError::MinOutputNotMet);
+    } else {
+        require!(params.max_amount_in > 0, DlmmError::MaxInputExceeded);
+    }
+
+    let pool = &mut ctx.accounts.pool;
+    let fee_bps = pool.base_fee_bps.saturating_add(pool.variable_fee_bps);
+    let direction: i32 = if params.a_to_b { 1 } else { -1 };
+    let mut cur_bin_id = pool.active_bin_id;
+    let bin_step_bps = pool.bin_step_bps;
+
+    let mut total_net: u64 = 0;
+    let mut total_out: u64 = 0;
+    let mut total_fee: u64 = 0;
+    let mut bins_traversed: u64 = 0;
+
+    for info in ctx.remaining_accounts.iter() {
+        let mut data = info.try_borrow_mut_data()?;
+        let bin_array: &mut BinArray = from_bytes_mut(&mut data[8..]);
+
+        if !bin_array.contains(cur_bin_id) {
+            continue;
+        }
+
+        loop {
+            if (params.a_to_b && cur_bin_id >= params.price_limit_bin_id)
+                || (!params.a_to_b && cur_bin_id <= params.price_limit_bin_id)
+            {
+                break;
+            }
+
+            if !bin_array.contains(cur_bin_id) {
+                break;
+            }
+
+            let price = bin_to_price(cur_bin_id, bin_step_bps)?;
+            let bin = bin_array.get_bin_mut(cur_bin_id)?;
+
+            if params.exact_in {
+                let used_gross = total_net.saturating_add(total_fee);
+                if used_gross >= params.amount {
+                    break;
+                }
+                let remaining_gross = params.amount.saturating_sub(used_gross);
+                let max_net = ((remaining_gross as u128)
+                    .checked_mul(10000u128)
+                    .ok_or(error!(DlmmError::MathOverflow))?
+                    .checked_div((10000u128).saturating_add(fee_bps as u128))
+                    .ok_or(error!(DlmmError::DivisionByZero))?) as u64;
+
+                if max_net == 0 {
+                    break;
+                }
+
+                let step = compute_swap_step(
+                    max_net,
+                    bin.amount_x,
+                    bin.amount_y,
+                    price,
+                    params.a_to_b,
+                    fee_bps,
+                )?;
+
+                if step.amount_out == 0 && !step.bin_depleted {
+                    cur_bin_id = cur_bin_id.saturating_add(direction);
+                    bins_traversed = bins_traversed.saturating_add(1);
+                    continue;
+                }
+
+                if params.a_to_b {
+                    bin.amount_y = bin
+                        .amount_y
+                        .checked_sub(step.amount_out)
+                        .ok_or(error!(DlmmError::MathOverflow))?;
+                } else {
+                    bin.amount_x = bin
+                        .amount_x
+                        .checked_sub(step.amount_out)
+                        .ok_or(error!(DlmmError::MathOverflow))?;
+                }
+
+                total_net = total_net
+                    .checked_add(step.amount_in_consumed)
+                    .ok_or(error!(DlmmError::MathOverflow))?;
+                total_out = total_out
+                    .checked_add(step.amount_out)
+                    .ok_or(error!(DlmmError::MathOverflow))?;
+                total_fee = total_fee
+                    .checked_add(step.fee_paid)
+                    .ok_or(error!(DlmmError::MathOverflow))?;
+                bins_traversed = bins_traversed.saturating_add(1);
+
+                if step.bin_depleted {
+                    cur_bin_id = cur_bin_id.saturating_add(direction);
+                } else {
+                    break;
+                }
+            } else {
+                let remaining_out = params.amount.saturating_sub(total_out);
+                if remaining_out == 0 {
+                    break;
+                }
+
+                let available = if params.a_to_b { bin.amount_y } else { bin.amount_x };
+
+                if available == 0 {
+                    cur_bin_id = cur_bin_id.saturating_add(direction);
+                    bins_traversed = bins_traversed.saturating_add(1);
+                    continue;
+                }
+
+                if params.a_to_b {
+                    let net_needed = ((remaining_out as u128)
+                        .checked_mul(Q64)
+                        .ok_or(error!(DlmmError::MathOverflow))?
+                        .checked_div(price)
+                        .ok_or(error!(DlmmError::DivisionByZero))?) as u64;
+
+                    if remaining_out <= available {
+                        let fee = apply_bps(net_needed, fee_bps)?;
+                        bin.amount_y = bin.amount_y.checked_sub(remaining_out).ok_or(error!(DlmmError::MathOverflow))?;
+                        total_net = total_net.checked_add(net_needed).ok_or(error!(DlmmError::MathOverflow))?;
+                        total_out = total_out.checked_add(remaining_out).ok_or(error!(DlmmError::MathOverflow))?;
+                        total_fee = total_fee.checked_add(fee).ok_or(error!(DlmmError::MathOverflow))?;
+                        break;
+                    } else {
+                        let fee = apply_bps(net_needed, fee_bps)?;
+                        bin.amount_y = 0;
+                        total_net = total_net.checked_add(net_needed).ok_or(error!(DlmmError::MathOverflow))?;
+                        total_out = total_out.checked_add(available).ok_or(error!(DlmmError::MathOverflow))?;
+                        total_fee = total_fee.checked_add(fee).ok_or(error!(DlmmError::MathOverflow))?;
+                        cur_bin_id = cur_bin_id.saturating_add(direction);
+                    }
+                } else {
+                    let net_needed = ((remaining_out as u128)
+                        .checked_mul(price)
+                        .ok_or(error!(DlmmError::MathOverflow))? >> 64) as u64;
+
+                    if remaining_out <= available {
+                        let fee = apply_bps(net_needed, fee_bps)?;
+                        bin.amount_x = bin.amount_x.checked_sub(remaining_out).ok_or(error!(DlmmError::MathOverflow))?;
+                        total_net = total_net.checked_add(net_needed).ok_or(error!(DlmmError::MathOverflow))?;
+                        total_out = total_out.checked_add(remaining_out).ok_or(error!(DlmmError::MathOverflow))?;
+                        total_fee = total_fee.checked_add(fee).ok_or(error!(DlmmError::MathOverflow))?;
+                        break;
+                    } else {
+                        let fee = apply_bps(net_needed, fee_bps)?;
+                        bin.amount_x = 0;
+                        total_net = total_net.checked_add(net_needed).ok_or(error!(DlmmError::MathOverflow))?;
+                        total_out = total_out.checked_add(available).ok_or(error!(DlmmError::MathOverflow))?;
+                        total_fee = total_fee.checked_add(fee).ok_or(error!(DlmmError::MathOverflow))?;
+                        cur_bin_id = cur_bin_id.saturating_add(direction);
+                    }
+                }
+                bins_traversed = bins_traversed.saturating_add(1);
+            }
+        }
+    }
+
+    if params.exact_in {
+        require!(total_out >= params.min_amount_out, DlmmError::MinOutputNotMet);
+    } else {
+        let total_in = total_net.checked_add(total_fee).ok_or(error!(DlmmError::MathOverflow))?;
+        require!(total_in <= params.max_amount_in, DlmmError::MaxInputExceeded);
+    }
+
+    let amount_in = total_net.checked_add(total_fee).ok_or(error!(DlmmError::MathOverflow))?;
+    let amount_out = total_out;
+
+    {
+        let pool = &mut ctx.accounts.pool;
+        pool.active_bin_id = cur_bin_id;
+
+        if params.a_to_b {
+            let protocol_share = apply_bps(total_fee, pool.protocol_fee_bps)?;
+            pool.pending_protocol_fees_x = pool
+                .pending_protocol_fees_x
+                .checked_add(protocol_share)
+                .ok_or(error!(DlmmError::MathOverflow))?;
+        } else {
+            let protocol_share = apply_bps(total_fee, pool.protocol_fee_bps)?;
+            pool.pending_protocol_fees_y = pool
+                .pending_protocol_fees_y
+                .checked_add(protocol_share)
+                .ok_or(error!(DlmmError::MathOverflow))?;
+        }
+    }
+
+    let pool_bump = ctx.accounts.pool.bump;
+    let mint_a = ctx.accounts.pool.token_mint_a;
+    let mint_b = ctx.accounts.pool.token_mint_b;
+
+    if params.a_to_b {
+        if amount_in > 0 {
+            token::transfer(
+                CpiContext::new(
+                    ctx.accounts.token_program.to_account_info(),
+                    Transfer {
+                        from: ctx.accounts.user_token_a.to_account_info(),
+                        to: ctx.accounts.token_vault_a.to_account_info(),
+                        authority: ctx.accounts.payer.to_account_info(),
+                    },
+                ),
+                amount_in,
+            )?;
+        }
+        if amount_out > 0 {
+            let seeds = &[b"pool", mint_a.as_ref(), mint_b.as_ref(), &[pool_bump]];
+            let signer_seeds = &[&seeds[..]];
+            token::transfer(
+                CpiContext::new_with_signer(
+                    ctx.accounts.token_program.to_account_info(),
+                    Transfer {
+                        from: ctx.accounts.token_vault_b.to_account_info(),
+                        to: ctx.accounts.user_token_b.to_account_info(),
+                        authority: ctx.accounts.pool.to_account_info(),
+                    },
+                    signer_seeds,
+                ),
+                amount_out,
+            )?;
+        }
+    } else {
+        if amount_in > 0 {
+            token::transfer(
+                CpiContext::new(
+                    ctx.accounts.token_program.to_account_info(),
+                    Transfer {
+                        from: ctx.accounts.user_token_b.to_account_info(),
+                        to: ctx.accounts.token_vault_b.to_account_info(),
+                        authority: ctx.accounts.payer.to_account_info(),
+                    },
+                ),
+                amount_in,
+            )?;
+        }
+        if amount_out > 0 {
+            let seeds = &[b"pool", mint_b.as_ref(), mint_a.as_ref(), &[pool_bump]];
+            let signer_seeds = &[&seeds[..]];
+            token::transfer(
+                CpiContext::new_with_signer(
+                    ctx.accounts.token_program.to_account_info(),
+                    Transfer {
+                        from: ctx.accounts.token_vault_a.to_account_info(),
+                        to: ctx.accounts.user_token_a.to_account_info(),
+                        authority: ctx.accounts.pool.to_account_info(),
+                    },
+                    signer_seeds,
+                ),
+                amount_out,
+            )?;
+        }
+    }
+
+    emit!(SwapEvent {
+        pool: ctx.accounts.pool.key(),
+        payer: ctx.accounts.payer.key(),
+        a_to_b: params.a_to_b,
+        amount_in,
+        amount_out,
+        fee: total_fee,
+        active_bin_after: cur_bin_id,
+        bins_traversed,
+    });
+
+    Ok(())
 }
--- programs/dlmm/src/math/swap_math.rs
+++ programs/dlmm/src/math/swap_math.rs
@@ -1,5 +1,7 @@
 use anchor_lang::prelude::*;
 use crate::errors::DlmmError;
+use super::fee_math::apply_bps;
+use super::fixed_point::Q64;
 
 pub struct SwapStepResult {
     pub amount_in_consumed: u64,
@@ -9,12 +11,127 @@ pub struct SwapStepResult {
 }
 
 pub fn compute_swap_step(
-    _amount_in_remaining: u64,
-    _bin_amount_x: u64,
-    _bin_amount_y: u64,
-    _bin_price_q64: u128,
-    _a_to_b: bool,
-    _fee_bps: u16,
+    remaining_net: u64,
+    bin_amount_x: u64,
+    bin_amount_y: u64,
+    bin_price_q64: u128,
+    a_to_b: bool,
+    fee_bps: u16,
 ) -> Result<SwapStepResult> {
-    Err(error!(DlmmError::NotImplemented))
+    if a_to_b {
+        let available = bin_amount_y;
+        if available == 0 {
+            return Ok(SwapStepResult {
+                amount_in_consumed: 0,
+                amount_out: 0,
+                fee_paid: 0,
+                bin_depleted: false,
+            });
+        }
+
+        let desired_out = ((remaining_net as u128).checked_mul(bin_price_q64)
+            .ok_or(error!(DlmmError::MathOverflow))? >> 64) as u64;
+
+        if desired_out <= available {
+            Ok(SwapStepResult {
+                amount_in_consumed: remaining_net,
+                amount_out: desired_out,
+                fee_paid: apply_bps(remaining_net, fee_bps)?,
+                bin_depleted: false,
+            })
+        } else {
+            let net_needed = ((available as u128)
+                .checked_mul(Q64)
+                .ok_or(error!(DlmmError::MathOverflow))?
+                .checked_div(bin_price_q64)
+                .ok_or(error!(DlmmError::DivisionByZero))?) as u64;
+
+            Ok(SwapStepResult {
+                amount_in_consumed: net_needed,
+                amount_out: available,
+                fee_paid: apply_bps(net_needed, fee_bps)?,
+                bin_depleted: true,
+            })
+        }
+    } else {
+        let available = bin_amount_x;
+        if available == 0 {
+            return Ok(SwapStepResult {
+                amount_in_consumed: 0,
+                amount_out: 0,
+                fee_paid: 0,
+                bin_depleted: false,
+            });
+        }
+
+        let desired_out = ((remaining_net as u128)
+            .checked_mul(Q64)
+            .ok_or(error!(DlmmError::MathOverflow))?
+            .checked_div(bin_price_q64)
+            .ok_or(error!(DlmmError::DivisionByZero))?) as u64;
+
+        if desired_out <= available {
+            Ok(SwapStepResult {
+                amount_in_consumed: remaining_net,
+                amount_out: desired_out,
+                fee_paid: apply_bps(remaining_net, fee_bps)?,
+                bin_depleted: false,
+            })
+        } else {
+            let net_needed = ((available as u128)
+                .checked_mul(bin_price_q64)
+                .ok_or(error!(DlmmError::MathOverflow))? >> 64) as u64;
+
+            Ok(SwapStepResult {
+                amount_in_consumed: net_needed,
+                amount_out: available,
+                fee_paid: apply_bps(net_needed, fee_bps)?,
+                bin_depleted: true,
+            })
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const Q64: u128 = 1u128 << 64;
+
+    #[test]
+    fn test_step_empty_bin() {
+        let r = compute_swap_step(1000, 100, 0, Q64, true, 0).unwrap();
+        assert_eq!(r.amount_out, 0);
+        assert!(!r.bin_depleted);
+    }
+
+    #[test]
+    fn test_step_partial_fill_a_to_b() {
+        let r = compute_swap_step(100, 500, 500, Q64, true, 0).unwrap();
+        assert_eq!(r.amount_out, 100);
+        assert_eq!(r.amount_in_consumed, 100);
+        assert!(!r.bin_depleted);
+    }
+
+    #[test]
+    fn test_step_full_deplete_a_to_b() {
+        let r = compute_swap_step(1000, 500, 50, Q64, true, 0).unwrap();
+        assert_eq!(r.amount_out, 50);
+        assert_eq!(r.amount_in_consumed, 50);
+        assert!(r.bin_depleted);
+    }
+
+    #[test]
+    fn test_step_partial_fill_b_to_a() {
+        let r = compute_swap_step(100, 500, 500, Q64, false, 0).unwrap();
+        assert_eq!(r.amount_out, 100);
+        assert!(!r.bin_depleted);
+    }
+
+    #[test]
+    fn test_step_with_fee() {
+        let r = compute_swap_step(1000, 5000, 5000, Q64, true, 100).unwrap();
+        assert_eq!(r.fee_paid, 10);
+        assert_eq!(r.amount_out, 1000);
+    }
 }
